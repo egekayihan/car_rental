d26020f47c3a5647285e959fa99338dbaa9f66cc
ek223gi <ek223gi@student.lnu.se>
2022-01-07 16:24:37 +0300

    Get methods done

--- src/MyCon.java
+++ src/MyCon.java
@@ -1,27 +1,106 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class MyCon {
-    public static void main(String[] args) {
-        try {
+    static Statement stmnt = makeConnection();
+    static ResultSet resultSet = null;
+
+    public static void main(String[] args) throws SQLException {
+        getShops();
+        System.out.println();
+
+        getCars();
+        System.out.println();
+
+        getClients();
+        System.out.println();
+
+        getEmployees();
+        System.out.println();
+
+        getInvoices();
+        System.out.println();
+
+    }
+
+    public static Statement makeConnection(){
+        try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/car_rental", "root", "root");
+                    "jdbc:mysql://localhost:3307/car_rental", "root", "root");
 
-            Statement stmnt = con.createStatement();
-            ResultSet rs = stmnt.executeQuery("select * from shop");
-
-
-            while (rs.next()){
-                System.out.println(rs.getInt(1) + " " +
-                        rs.getString(2) + " " +
-                        rs.getString(3) /*+ " " +*/
-                       /* rs.getDate(4)*/);
-            }
+            stmnt = con.createStatement();
         } catch(Exception ex) {
             System.out.println("Exception");
+        }
+        return stmnt;
+    }
+
+    public static void getShops() throws SQLException {
+        resultSet = stmnt.executeQuery("select * from shop");
+        if (resultSet.next())
+            System.out.println("SHOPS");
+
+        while (resultSet.next()){
+            System.out.println("ID: " + resultSet.getInt(1) + "    City: " +
+                    resultSet.getString(2) + "    Address: " +
+                    resultSet.getString(3));
+        }
+    }
+
+    public static void getCars() throws SQLException {
+        resultSet = stmnt.executeQuery("select * from car");
+        if (resultSet.next())
+            System.out.println("CARS");
+
+        while (resultSet.next()){
+            System.out.println("ID: " + resultSet.getInt(1) + "    Brand: " +
+                    resultSet.getString(2) + "    Model: " +
+                    resultSet.getString(3) + "    Production year: " +
+                    resultSet.getString(4) + "    Fee: " +
+                    resultSet.getString(5) + "    Shop ID: " );
+        }
+    }
+
+    public static void getInvoices() throws SQLException {
+        resultSet = stmnt.executeQuery("select * from invoice");
+        if (resultSet.next())
+            System.out.println("INVOICES");
+
+        while (resultSet.next()){
+            System.out.println("ID: " + resultSet.getInt(1) + "    Starting Date:  " +
+                    resultSet.getString(2) + "    Ending Date: " +
+                    resultSet.getString(3) + "    Cost: " +
+                    resultSet.getString(3) + "    Client Personal Number: " +
+                    resultSet.getString(3) + "    Employee ID: " +
+                    resultSet.getString(3) + "    Car ID: " +
+                    resultSet.getString(3));
+        }
+    }
+
+    public static void getClients() throws SQLException {
+        resultSet = stmnt.executeQuery("select * from client");
+        if (resultSet.next())
+            System.out.println("CLIENTS");
+
+        while (resultSet.next()){
+            System.out.println("Personal Number: " + resultSet.getInt(1) + "    First Name: " +
+                    resultSet.getString(2) + "    Last Name: " +
+                    resultSet.getString(3) + "    Address: " +
+                    resultSet.getString(4));
+        }
+    }
+
+    public static void getEmployees() throws SQLException {
+        resultSet = stmnt.executeQuery("select * from employee");
+        if (resultSet.next())
+            System.out.println("EMPLOYEES");
+
+        while (resultSet.next()){
+            System.out.println("ID: " + resultSet.getInt(1) + "    First Name: " +
+                    resultSet.getString(2) + "    Last Name: " +
+                    resultSet.getString(3) + "    Title: " +
+                    resultSet.getString(4) + "    Shop ID: " +
+                    resultSet.getString(5));
         }
     }
 }
